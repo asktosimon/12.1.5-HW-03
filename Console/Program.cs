10c47a57d5b6df52f8564909895f12900811b217
Andrey Simanovich <asktosimon@yandex.ru>
2022-07-25 17:27:27 +0300

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("How many elements there will be in array?");
+var countString = Console.ReadLine();
+
+int count = Convert.ToInt32(countString);
+
+var resultArray = new string[count];
+
+for(int i = 0; i < count; i++)
+{
+    var str = Console.ReadLine();
+
+    resultArray[i] = str ?? "";
+}
+
+Console.WriteLine("All of elements are written");
